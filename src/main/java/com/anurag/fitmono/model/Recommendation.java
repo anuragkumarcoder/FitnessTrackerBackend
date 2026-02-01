3abb911cdff1eb926394183a928165fbb5470751
anurag <anuragkumar78702685001@gmail.com>
2026-02-01 12:50:59 +0530

    inital processes user and activity tracking done

--- src/main/java/com/anurag/fitmono/model/Recommendation.java
+++ src/main/java/com/anurag/fitmono/model/Recommendation.java
@@ -2,9 +2,12 @@ package com.anurag.fitmono.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
@@ -13,7 +16,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Builder
 public class Recommendation {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -36,7 +39,9 @@ public class Recommendation {
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "json")
     private List<String> safety;
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
 }
